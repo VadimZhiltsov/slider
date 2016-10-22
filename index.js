b77c1ba22dd3be73882d34b6b71ab9dec4d201ac
cgenchevsky <c.genchecsky@sigma.software>
2016-10-22 15:24:25 +0300

    before make minification

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 (function () {
+	var intervalID = autoplay();
 
 	function getRandom(min, max) {
 		return Math.floor(min + (max-min) * Math.random());
@@ -7,7 +8,7 @@
 	function setSlide(number) {
 		var activeSlide = $('.slider__list').children(':nth-child(' + number + ')')
 		var list = $('.slider__list');
-		list.css('transform',  'translateX('+ (-100 * number ) + '%)');
+		list.css('transform',  'translateX('+ (-100 * (number - 1) ) + '%)');
 		list.children().removeClass('active');
 		activeSlide.addClass('active');
 		console.log('SLIDE HAS BEEN CHANGED');
@@ -16,18 +17,53 @@
 	function getActiveNumber() {
 		return $('.slider__list li.active').index() + 1;
 	}
+	
+	function getSlidesCount() {
+		return $('.slider__list li').length;
+	}
 
 	function nextSlide() {
 		var activeSlideNumber = getActiveNumber();
-		nextSlideNumber = activeSlideNumber + 1;
+		var nextSlideNumber = activeSlideNumber + 1;
+		var slidesCount = getSlidesCount()
 
-		setSlide(nextSlideNumber);
+		if (nextSlideNumber <= slidesCount) {
+			setSlide(nextSlideNumber);
+		}
 
 	}
 
-	setInterval(function() {
-		var slide = getRandom(0, 5);
-		nextSlide()
-	}, 4000)
+	function prevSlide() {
+		var activeSlideNumber = getActiveNumber();
+		var prevSlideNumber = activeSlideNumber - 1;
+		var slidesCount = getSlidesCount()
 
+		if (prevSlideNumber > 0) {
+			setSlide(prevSlideNumber);
+		}
+	}
+
+	/*
+		returns intervalId
+	*/
+	function autoplay() {
+		return setInterval(function() {
+			nextSlide()
+		}, 2000);
+	}
+
+	$('.slider__prev-button').on('click', function() {
+		prevSlide();
+		clearInterval(intervalID);
+	});
+
+	$(document).on('click', '.slider__next-button', function() {
+		nextSlide();
+		clearInterval(intervalID);
+	});
+
+	$('.slider__autoplay-button').on('click', function() {
+		clearInterval(intervalID);
+		intervalID = autoplay();
+	});
 })();
